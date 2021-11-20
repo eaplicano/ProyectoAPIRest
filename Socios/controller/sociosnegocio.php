0873ff394b35ea529becb5e1e5d2c850198b6a6d
eaplicano <eliud.aplicano@unah.hn>
2021-11-20 14:05:23 -0600

    Modificaion de subcarpetas por entidad

--- Socios/controller/sociosnegocio.php
+++ Socios/controller/sociosnegocio.php
@@ -1,8 +1,19 @@
 <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+        header('Access-Control-Allow-Headers: token, Content-Type');
+        header('Access-Control-Max-Age: 1728000');
+        header('Content-Length: 0');
+        header('Content-Type: text/plain');
+        die();
+    }
+    header('Access-Control-Allow-Origin: *');  
     header('Content-Type: application/json');
 
-    require_once("../config/conexion.php");
-    require_once("../models/Sociosnegocio.php");
+    require_once("../../config/conexion.php");
+    require_once("../../Socios/models/Sociosnegocio.php");
+    
     $socios = new Sociosnegocio();
 
     $body = json_decode(file_get_contents("php://input"),true);
@@ -30,7 +41,7 @@
         break;
 
         case "DeleteSocio":
-            $datos=$socios->x($body["id"]);
+            $datos=$socios->delete_socionegocio($body["id"]);
             echo json_encode("Socio Borrado");
         break;
     }
